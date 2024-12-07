8d2ba0c92a6b307b4b3388f7968abfa1b7527ecf
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2024-12-07 19:21:08 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,3 +7,9 @@ sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+
+修改主机名字，把 OpenWRT 修改你喜欢的就行（不能纯数字或者使用中文）
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWRT'' package/lean/default-settings/files/zzz-default-settings
+
+设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
